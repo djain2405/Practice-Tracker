a65a3f13bcd832ceeb9343c76691c29b0fc06add
Divya Jain <divyajain2405@gmail.com>
2021-02-27 21:41:34 -0800

    Add the swiftui basic project

---
+++ Keep-It-Going/Keep-It-Going/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Keep-It-Going
+//
+//  Created by Divya Jain on 2/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Keep-It-Going/Keep-It-Going/Keep_It_GoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Keep_It_GoingApp.swift
+//  Keep-It-Going
+//
+//  Created by Divya Jain on 2/27/21.
+//
+
+import SwiftUI
+
+@main
+struct Keep_It_GoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
